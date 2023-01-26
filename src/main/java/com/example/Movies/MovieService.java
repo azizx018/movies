a5b59344f00e5fecd3cf8c8aa81d597bb88f315f
Aziz, Chiara <chiara_aziz@optum.com>
2023-01-26 14:50:20 -0500

    added get single movie via the id

--- src/main/java/com/example/Movies/MovieService.java
+++ src/main/java/com/example/Movies/MovieService.java
@@ -1,10 +1,12 @@
 package com.example.Movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,5 +16,8 @@ public class MovieService {
     public List<Movie> allMovies() {
        return movieRepository.findAll();
 
+    }
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 }
